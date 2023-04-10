fb6531a13253b22a721625ab683ef0b95cd0bbe7
EvertonSouzaFerreira <everton.souzaferreira@outlook.com>
2023-04-10 12:17:46 +0200

    firt commit

---
+++ app.js
@@ -0,0 +1,142 @@
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// const { extractData } = require("./index");
+
+// app.get("/data", async (req, res) => {
+//   const data = await extractData();
+//   res.json(data);
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
+
+// const express = require("express");
+// const mongoose = require("mongoose");
+// const { extractData } = require("./index");
+
+// const app = express();
+// const port = 3000;
+
+// // Conectar ao banco de dados
+// mongoose.connect("mongodb://localhost:27017/meu_banco_de_dados", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// // Criar esquema para modelo de dados
+// const respostaSchema = new mongoose.Schema({
+//   data: Object,
+// });
+
+// // Criar modelo de dados
+// const Resposta = mongoose.model("Resposta", respostaSchema);
+
+// // Função para salvar a resposta no banco de dados
+// const salvarResposta = (data) => {
+//   const novaResposta = new Resposta({ data: data });
+//   novaResposta
+//     .save()
+//     .then((respostaSalva) =>
+//       console.log(`Resposta salva no banco de dados: ${respostaSalva}`)
+//     )
+//     .catch((err) => console.error(err));
+// };
+
+// // Executar a extração de dados a cada 20 segundos
+// setInterval(async () => {
+//   const data = await extractData();
+//   salvarResposta(data);
+// }, 20000);
+
+// // Rota para obter a última resposta salva no banco de dados
+// app.get("/data", async (req, res) => {
+//   try {
+//     const ultimaResposta = await Resposta.findOne().sort({ $natural: -1 });
+//     res.json(ultimaResposta.data);
+//   } catch (err) {
+//     console.error(err);
+//     res
+//       .status(500)
+//       .send("Erro ao obter a última resposta salva no banco de dados.");
+//   }
+// });
+
+// // Iniciar o servidor
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
+
+const express = require("express");
+const mongoose = require("mongoose");
+const { extractData } = require("./index");
+const dotenv = require("dotenv");
+
+const app = express();
+const port = 3000;
+
+// Conectar ao banco de dados
+// mongoose.connect("mongodb://localhost:27017/meu_banco_de_dados", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+//mongodb+srv://everton:MongoDb@cluster0.h5v7cj1.mongodb.net/amazona?retryWrites=true&w=majority
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Criar esquema para modelo de dados
+// const respostaSchema = new mongoose.Schema({
+//   data: Object,
+// });
+
+const respostaSchema = new mongoose.Schema(
+  {
+    data: Object,
+  },
+  { collection: "somaPedras" }
+);
+
+// Criar modelo de dados
+const Resposta = mongoose.model("Resposta", respostaSchema);
+
+// Definir tarefa a ser executada a cada 20 segundos
+const CronJob = require("cron").CronJob;
+const job = new CronJob("*/20 * * * * *", async () => {
+  const data = await extractData();
+
+  // Buscar resposta existente no banco de dados
+  const resposta = await Resposta.findOne();
+
+  if (resposta) {
+    // Atualizar resposta existente com os dados extraídos
+    resposta.data = data;
+    await resposta.save();
+    console.log(`Resposta atualizada no banco de dados: ${resposta}`);
+  } else {
+    // Criar uma nova resposta com os dados extraídos
+    const novaResposta = new Resposta({ data: data });
+
+    // Salvar a nova resposta no banco de dados
+    await novaResposta.save();
+    console.log(`Resposta salva no banco de dados: ${novaResposta}`);
+  }
+});
+
+job.start();
+
+// Rota para buscar a resposta mais recente no banco de dados
+app.get("/data", async (req, res) => {
+  const resposta = await Resposta.findOne();
+  res.json(resposta);
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
+
+//password: g9fhOJOscFMHZvUG
---
+++ index.js
@@ -0,0 +1,590 @@
+// const puppeteer = require("puppeteer");
+// const fs = require("fs");
+// let lastTime = null;
+// let lastColor = null;
+// let timeBeforeUnload = null;
+// let nextObj = null;
+
+// const URL = "https://www.historicosblaze.com/br/blaze/doubles";
+
+// async function extractData() {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
+
+//   await page.goto(URL, { timeout: 70000 });
+
+//   const color = await page.$$(".entries .d-none .color-table");
+//   const numPedra = await page.$$(".entries .d-none .number-table");
+//   const hora = await page.$$(".entries .d-none .minute-table");
+
+//   const results = [];
+
+//   page.on("beforeunload", () => {
+//     timeBeforeUnload = Date.now();
+//   });
+
+//   for (
+//     let i = 0;
+//     i < 3 && i < color.length && i < numPedra.length && i < hora.length;
+//     i++
+//   ) {
+//     const time = await hora[i].evaluate((node) => node.textContent);
+//     const currColor = await color[i].evaluate((node) => node.textContent);
+//     const currNumPedra = await numPedra[i].evaluate((node) => node.textContent);
+
+//     if (currColor === "Red" && time !== lastTime && time !== timeBeforeUnload) {
+//       console.log("Veio um Red");
+//       if (nextObj) {
+//         // Obtém a hora do objeto atual e o numPedra do próximo objeto e soma
+//         const sum = parseInt(time) + parseInt(nextObj.numPedra);
+//         results[i - 1].sumRed = sum;
+//       }
+//     } else {
+//       console.log("ainda nao e uma Branca");
+//     }
+
+//     results.push({
+//       color: currColor,
+//       numPedra: currNumPedra,
+//       hora: time,
+//     });
+
+//     // Armazena o objeto atual na variável nextObj
+//     nextObj = {
+//       color: currColor,
+//       numPedra: currNumPedra,
+//       hora: time,
+//     };
+
+//     lastTime = time;
+//     lastColor = currColor;
+//   }
+//   console.log(results);
+
+//   fs.writeFile("blazer.json", JSON.stringify(results, null, 2), (err) => {
+//     if (err) throw new Error("something went wrong");
+
+//     console.log("well done");
+//   });
+
+//   await browser.close();
+// }
+
+// (async () => {
+//   while (true) {
+//     await extractData();
+//     await new Promise((resolve) => setTimeout(resolve, 25000));
+//   }
+// })();
+
+// const puppeteer = require("puppeteer");
+// const fs = require("fs");
+// let lastTime = null;
+// let lastColor = null;
+// let timeBeforeUnload = null;
+// let redTime = null;
+
+// const URL = "https://www.historicosblaze.com/br/blaze/doubles";
+
+// async function extractData() {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
+
+//   await page.goto(URL, { timeout: 70000 });
+
+//   const color = await page.$$(".entries .d-none .color-table");
+//   const numPedra = await page.$$(".entries .d-none .number-table");
+//   const hora = await page.$$(".entries .d-none .minute-table");
+
+//   const results = [];
+
+//   page.on("beforeunload", () => {
+//     timeBeforeUnload = Date.now();
+//   });
+
+//   for (
+//     let i = 0;
+//     i < 3 && i < color.length && i < numPedra.length && i < hora.length;
+//     i++
+//   ) {
+//     const time = await hora[i].evaluate((node) => node.textContent);
+//     const currColor = await color[i].evaluate((node) => node.textContent);
+//     const currNumPedra = await numPedra[i].evaluate((node) => node.textContent);
+
+//     if (currColor === "Red" && time !== lastTime && time !== timeBeforeUnload) {
+//       console.log("Veio um Red");
+//       redTime = time;
+//     } else if (
+//       currColor === "Black" &&
+//       lastColor === "Red" &&
+//       redTime !== null
+//     ) {
+//       const redTimeSplit = redTime.split(":");
+//       const redHour = parseInt(redTimeSplit[0], 10);
+//       const redMin = parseInt(redTimeSplit[1], 10);
+//       const currNumPedraNumber = parseInt(currNumPedra, 10);
+
+//       const currTimeSplit = time.split(":");
+//       const currHour = parseInt(currTimeSplit[0], 10);
+//       const currMin = parseInt(currTimeSplit[1], 10);
+
+//       const newMin = (redMin + currNumPedraNumber) % 60;
+//       const extraHour = Math.floor((redMin + currNumPedraNumber) / 60);
+
+//       const newHour = (redHour + currHour + extraHour) % 24;
+
+//       const sumTime = `${newHour.toString().padStart(2, "0")}:${newMin
+//         .toString()
+//         .padStart(2, "0")}`;
+
+//       console.log(`Soma realizada: ${redTime} + ${currNumPedra} = ${sumTime}`);
+
+//       redTime = null;
+//     } else {
+//       console.log("ainda nao e uma Branca");
+//     }
+
+//     results.push({
+//       color: currColor,
+//       numPedra: currNumPedra,
+//       hora: time,
+//     });
+//     lastTime = time;
+//     lastColor = currColor;
+//   }
+//   console.log(results);
+//   fs.writeFile("blazer.json", JSON.stringify(results, null, 2), (err) => {
+//     if (err) throw new Error("something went wrong");
+
+//     console.log("well done");
+//   });
+
+//   await browser.close();
+// }
+
+// (async () => {
+//   while (true) {
+//     await extractData();
+//     await new Promise((resolve) => setTimeout(resolve, 25000));
+//   }
+// })();
+
+// const puppeteer = require("puppeteer");
+// const fs = require("fs");
+
+// let redStones = [];
+// let sums = [];
+
+// let lastTime = null;
+// let lastColor = null;
+// let timeBeforeUnload = null;
+// let redTime = null;
+// let lastRedTime = null;
+
+// const URL = "https://www.historicosblaze.com/br/blaze/doubles";
+
+// async function extractData() {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
+
+//   await page.goto(URL, { timeout: 70000 });
+
+//   const color = await page.$$(".entries .d-none .color-table");
+//   const numPedra = await page.$$(".entries .d-none .number-table");
+//   const hora = await page.$$(".entries .d-none .minute-table");
+
+//   const results = [];
+
+//   page.on("beforeunload", () => {
+//     timeBeforeUnload = Date.now();
+//   });
+
+//   for (
+//     let i = 0;
+//     i < 3 && i < color.length && i < numPedra.length && i < hora.length;
+//     i++
+//   ) {
+//     const time = await hora[i].evaluate((node) => node.textContent);
+//     const currColor = await color[i].evaluate((node) => node.textContent);
+//     const currNumPedra = await numPedra[i].evaluate((node) => node.textContent);
+
+//     if (currColor === "Red") {
+//       console.log(`Pedra vermelha encontrada: ${currNumPedra} (${time})`);
+//       lastRedTime = time;
+//     } else if (currColor === "Black" && lastRedTime !== null) {
+//       console.log(`Pedra preta encontrada: ${currNumPedra} (${time})`);
+
+//       const redTimeSplit = lastRedTime.split(":");
+//       const redHour = parseInt(redTimeSplit[0], 10);
+//       const redMin = parseInt(redTimeSplit[1], 10);
+//       const currNumPedraNumber = parseInt(currNumPedra, 10);
+
+//       const currTimeSplit = time.split(":");
+//       const currHour = parseInt(currTimeSplit[0], 10);
+//       const currMin = parseInt(currTimeSplit[1], 10);
+
+//       const newMin = (redMin + currNumPedraNumber) % 60;
+//       const extraHour = Math.floor((redMin + currNumPedraNumber) / 60);
+
+//       const newHour = (redHour + currHour + extraHour) % 24;
+
+//       const sumTime = `${newHour.toString().padStart(2, "0")}:${newMin
+//         .toString()
+//         .padStart(2, "0")}`;
+
+//       console.log(
+//         `Soma realizada: ${lastRedTime} + ${currNumPedra} = ${sumTime}`
+//       );
+
+//       results.push({
+//         color: currColor,
+//         numPedra: currNumPedra,
+//         hora: time,
+//         soma: sumTime,
+//       });
+
+//       lastRedTime = null;
+//     } else {
+//       console.log(`Pedra ignorada: ${currNumPedra} (${time})`);
+//     }
+//   }
+//   console.log(results);
+
+//   fs.writeFile("blazer.json", JSON.stringify(results, null, 2), (err) => {
+//     if (err) throw new Error("something went wrong");
+
+//     console.log("well done");
+//   });
+
+//   await browser.close();
+// }
+
+// (async () => {
+//   while (true) {
+//     await extractData();
+//     await new Promise((resolve) => setTimeout(resolve, 25000));
+//   }
+// })();
+
+// const puppeteer = require("puppeteer");
+// const fs = require("fs");
+
+// let redStones = [];
+// let sums = [];
+
+// let lastTime = null;
+// let lastColor = null;
+// let timeBeforeUnload = null;
+// let redTime = null;
+// let lastRedTime = null;
+
+// const URL = "https://www.historicosblaze.com/br/blaze/doubles";
+
+// async function extractData() {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
+
+//   await page.goto(URL, { timeout: 70000 });
+
+//   const color = await page.$$(".entries .d-none .color-table");
+//   const numPedra = await page.$$(".entries .d-none .number-table");
+//   const hora = await page.$$(".entries .d-none .minute-table");
+
+//   const results = [];
+
+//   page.on("beforeunload", () => {
+//     timeBeforeUnload = Date.now();
+//   });
+
+//   for (
+//     let i = 0;
+//     i < 3 && i < color.length && i < numPedra.length && i < hora.length;
+//     i++
+//   ) {
+//     const time = await hora[i].evaluate((node) => node.textContent);
+//     const currColor = await color[i].evaluate((node) => node.textContent);
+//     const currNumPedra = await numPedra[i].evaluate((node) => node.textContent);
+
+//     if (currColor === "Red") {
+//       console.log(`Pedra vermelha encontrada: ${currNumPedra} (${time})`);
+//       lastRedTime = time;
+//     } else if (currColor === "Black" && lastRedTime !== null) {
+//       console.log(`Pedra preta encontrada: ${currNumPedra} (${time})`);
+
+//       const redTimeSplit = lastRedTime.split(":");
+//       const redHour = parseInt(redTimeSplit[0], 10);
+//       const redMin = parseInt(redTimeSplit[1], 10);
+//       const currNumPedraNumber = parseInt(currNumPedra, 10);
+
+//       const currTimeSplit = time.split(":");
+//       const currHour = parseInt(currTimeSplit[0], 10);
+//       const currMin = parseInt(currTimeSplit[1], 10);
+
+//       const newMin = (redMin + currNumPedraNumber) % 60;
+//       const extraHour = Math.floor((redMin + currNumPedraNumber) / 60);
+
+//       const newHour = (redHour + currHour + extraHour) % 24;
+
+//       const sumTime = `${newHour.toString().padStart(2, "0")}:${newMin
+//         .toString()
+//         .padStart(2, "0")}`;
+
+//       console.log(
+//         `Soma realizada: ${lastRedTime} + ${currNumPedra} = ${sumTime}`
+//       );
+
+//       results.push({
+//         color: currColor,
+//         numPedra: currNumPedra,
+//         hora: time,
+//         soma: sumTime,
+//       });
+
+//       lastRedTime = null;
+//     } else {
+//       console.log(`Pedra ignorada: ${currNumPedra} (${time})`);
+//     }
+//   }
+//   console.log(results);
+
+//   fs.writeFile("blazer.json", JSON.stringify(results, null, 2), (err) => {
+//     if (err) throw new Error("something went wrong");
+
+//     console.log("well done");
+//   });
+
+//   await browser.close();
+// }
+
+// (async () => {
+//   while (true) {
+//     await extractData();
+//     await new Promise((resolve) => setTimeout(resolve, 25000));
+//   }
+// })();
+
+// const puppeteer = require("puppeteer");
+// const fs = require("fs");
+
+// let redStones = [];
+// let sums = [];
+
+// let lastTime = null;
+// let lastColor = null;
+// let timeBeforeUnload = null;
+// let redTime = null;
+// let lastRedTime = null;
+
+// const URL = "https://www.historicosblaze.com/br/blaze/doubles";
+
+// async function extractData() {
+//   const browser = await puppeteer.launch({ headless: false });
+//   const page = await browser.newPage();
+
+//   await page.goto(URL, { timeout: 70000 });
+
+//   const color = await page.$$(".entries .d-none .color-table");
+//   const numPedra = await page.$$(".entries .d-none .number-table");
+//   const hora = await page.$$(".entries .d-none .minute-table");
+
+//   const results = [];
+
+//   page.on("beforeunload", () => {
+//     timeBeforeUnload = Date.now();
+//   });
+
+//   for (
+//     let i = 0;
+//     i < 3 && i < color.length && i < numPedra.length && i < hora.length;
+//     i++
+//   ) {
+//     const time = await hora[i].evaluate((node) => node.textContent);
+//     const currColor = await color[i].evaluate((node) => node.textContent);
+//     const currNumPedra = await numPedra[i].evaluate((node) => node.textContent);
+
+//     if (currColor === "Red") {
+//       console.log(`Pedra vermelha encontrada: ${currNumPedra} (${time})`);
+//       if (!redTime) redTime = time;
+//     } else if (currColor === "Black" && redTime !== null) {
+//       console.log(`Pedra preta encontrada: ${currNumPedra} (${time})`);
+
+//       const redTimeSplit = redTime.split(":");
+//       const redHour = parseInt(redTimeSplit[0], 10);
+//       const redMin = parseInt(redTimeSplit[1], 10);
+//       const currNumPedraNumber = parseInt(currNumPedra, 10);
+
+//       const currTimeSplit = time.split(":");
+//       const currHour = parseInt(currTimeSplit[0], 10);
+//       const currMin = parseInt(currTimeSplit[1], 10);
+
+//       const newMin = (redMin + currNumPedraNumber) % 60;
+//       const extraHour = Math.floor((redMin + currNumPedraNumber) / 60);
+//       console.log(`redHour: ${redHour}`);
+//       console.log(`redMin: ${redMin}`);
+//       console.log(`currHour: ${currHour}`);
+//       console.log(`currMin: ${currMin}`);
+//       console.log(`currNumPedraNumber: ${currNumPedraNumber}`);
+//       const newHour = (redHour + extraHour) % 24;
+
+//       const sumTime = `${newHour.toString().padStart(2, "0")}:${newMin
+//         .toString()
+//         .padStart(2, "0")}`;
+
+//       console.log(`Soma realizada: ${redTime} + ${currNumPedra} = ${sumTime}`);
+
+//       results.push({
+//         color: currColor,
+//         numPedra: currNumPedra,
+//         hora: time,
+//         soma: sumTime,
+//       });
+
+//       redTime = null;
+//     } else {
+//       console.log(`Pedra ignorada: ${currNumPedra} (${time})`);
+//     }
+//   }
+//   console.log(results);
+
+//   fs.writeFile("blazer.json", JSON.stringify(results, null, 2), (err) => {
+//     if (err) throw new Error("something went wrong");
+
+//     console.log("well done");
+//   });
+
+//   await browser.close();
+// }
+
+// (async () => {
+//   while (true) {
+//     await extractData();
+//     await new Promise((resolve) => setTimeout(resolve, 25000));
+//   }
+// })();
+
+const express = require("express");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const app = express();
+
+let redStones = [];
+let sums = [];
+let data = null;
+let processedBlackStones = new Set();
+let timeBeforeUnload = null;
+let processedRedStones = [];
+let lastRedTime = null;
+let redStoneFound = false; // variável adicionada para controlar se a pedra vermelha já foi encontrada e somada
+let ColorPedra = "Red";
+
+const URL = "https://www.historicosblaze.com/br/blaze/doubles";
+
+async function extractData() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(URL, { timeout: 70000 });
+
+  const color = await page.$$(".entries .d-none .color-table");
+  const numPedra = await page.$$(".entries .d-none .number-table");
+  const hora = await page.$$(".entries .d-none .minute-table");
+
+  const results = [];
+
+  page.on("beforeunload", () => {
+    timeBeforeUnload = Date.now();
+  });
+
+  const firstColor = await color[0].evaluate((node) => node.textContent);
+
+  for (
+    let i = 0;
+    i < 3 && i < color.length && i < numPedra.length && i < hora.length;
+    i++
+  ) {
+    const time = await hora[i].evaluate((node) => node.textContent);
+    const currColor = await color[i].evaluate((node) => node.textContent);
+    const currNumPedra = await numPedra[i].evaluate((node) => node.textContent);
+
+    if (currColor === ColorPedra && lastRedTime === null) {
+      console.log(
+        `Primeira pedra Branca encontrada: ${currNumPedra} (${time})`
+      );
+      lastRedTime = time;
+      processedRedStones.push(currNumPedra);
+    } else if (currColor === ColorPedra && lastRedTime !== null) {
+      console.log(`Nova pedra Branca encontrada: ${currNumPedra} (${time})`);
+      lastRedTime = time;
+      processedRedStones.push(currNumPedra);
+    } else if (
+      currColor === "Black" &&
+      lastRedTime !== null &&
+      time >= lastRedTime &&
+      !processedBlackStones.has(currNumPedra) && // verificar se a pedra já foi processada
+      !redStoneFound // verifica se a pedra vermelha ainda não foi encontrada e somada
+    ) {
+      console.log(`Pedra preta encontrada: ${currNumPedra} (${time})`);
+
+      const redTimeSplit = lastRedTime.split(":");
+      const redHour = parseInt(redTimeSplit[0], 10);
+      const redMin = parseInt(redTimeSplit[1], 10);
+      const currNumPedraNumber = parseInt(currNumPedra, 10);
+
+      const currTimeSplit = time.split(":");
+      const currHour = parseInt(currTimeSplit[0], 10);
+      const currMin = parseInt(currTimeSplit[1], 10);
+
+      const newMin = (redMin + currNumPedraNumber) % 60;
+      const extraHour = Math.floor((redMin + currNumPedraNumber) / 60);
+
+      const newHour = (redHour + extraHour) % 24;
+
+      //esta soma so pode acontecer se o
+      const sumTime = `${newHour.toString().padStart(2, "0")}:${newMin
+        .toString()
+        .padStart(2, "0")}`;
+
+      console.log(
+        `Soma realizada: ${lastRedTime} + ${currNumPedra} = ${sumTime}`
+      );
+
+      results.push({
+        color: currColor,
+        numPedra: currNumPedra,
+        hora: lastRedTime,
+        soma: sumTime,
+      });
+
+      lastRedTime = null;
+      contador = true;
+    } else {
+      console.log(`Pedra ignorada: ${currNumPedra} (${time})`);
+    }
+  }
+  console.log(results);
+  if (results.length > 0) {
+    sums.push(...results);
+  }
+
+  fs.writeFile("blazer.json", JSON.stringify(sums, null, 2), (err) => {
+    if (err) throw new Error("something went wrong");
+
+    console.log("well done");
+  });
+
+  await browser.close();
+  return sums;
+}
+
+module.exports = { extractData };
+// (async () => {
+//   while (true) {
+//     await extractData();
+//     await new Promise((resolve) => setTimeout(resolve, 25000));
+//   }
+// })();
+
+// dois problemas primeiro se tiver uma pedra vermelha dentro do array que foi que ja foi somado e apare
+// um novo vermelho ele esta iginorando o vermelho
+
+//2 ele nao esta somando com a proxima bleck
